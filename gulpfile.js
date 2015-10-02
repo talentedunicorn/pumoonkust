d5c2a95f919a8ffe2ffe4d577b1308ec6e63422f
Ezekiel Mangalu <emangalu@gmail.com>
2015-10-02 18:41:58 +0800

    Setup project folders & initial files

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+/* =====================
+	Pumoonkust gulpfile.
+   ===================== */
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+
+gulp.task('sass', function() {
+	var conf = {
+		sourcemap: true,
+	};
+	
+	return sass('./sass/*.scss', conf)
+		.on('error', sass.logError)
+		.pipe(gulp.dest('./css'));
+});
+
+// Watch
+gulp.task('sass:watch', function() {
+	gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+// Default
+gulp.task('default', function() {
+});
